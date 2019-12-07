e9c284ca3adc0eee78ecea0ade9d783c9bbbae7b
UPN <UPN@ULNLAB1418>
2019-12-07 12:02:29 -0500

    Imprimiendo mensaje de año nuevo

---
+++ java45/src/java45/Java45.java
@@ -0,0 +1,11 @@
+package java45;
+
+import javax.swing.JOptionPane;
+
+public class Java45 {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"Feliz año nuevo 2020");
+    } 
+}
